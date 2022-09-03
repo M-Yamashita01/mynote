32abee16e4906de58f3a880fea17f375e80bf184
Masato Yamashita <4653960+M-Yamashita01@users.noreply.github.com>
2022-09-03 12:53:30 +0900

    Create sign in page in frontend.

---
+++ mynote-frontend/pages/signin/index.vue
@@ -0,0 +1,115 @@
+<template>
+  <v-app id="inspire">
+    <v-app-bar app>
+
+      <v-toolbar-title>MyNote</v-toolbar-title>
+      <div class="flex-grow-1"></div>
+
+      <v-btn to="/login"
+        text
+      >
+        ログイン
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <v-container>
+        <v-row
+          no-gutters
+          style="flex-wrap: nowrap;"
+        >
+          <v-col
+            cols="6"
+            class="flex-grow-0 flex-shrink-0"
+          >
+            サンプル
+          </v-col>
+          <v-col
+            cols="6"
+            class="flex-grow-0 flex-shrink-0"
+          >
+            <v-container>
+              <v-sheet
+                color="white"
+                elevation="1"
+                height="400"
+                rounded
+                width="300"
+              >
+                <v-container>
+                  <v-form v-model="valid" @submit.prevent="SignIn">
+                    <p class="text-center">
+                      サインイン
+                    </p>
+
+                    <v-text-field
+                      v-model="firstName"
+                      :rules="nameRules"
+                      label="FirstName"
+                      required
+                    ></v-text-field>
+
+                    <v-text-field
+                      v-model="lastName"
+                      :rules="nameRules"
+                      label="LastName"
+                      required
+                    ></v-text-field>
+
+                    <v-text-field
+                      v-model="email"
+                      :rules="emailRules"
+                      label="E-mail"
+                      required
+                    ></v-text-field>
+
+                    <v-text-field
+                      v-model="password"
+                      :rules="passwordRules"
+                      label="Password"
+                      required
+                    ></v-text-field>
+
+                    <v-btn block color="primary" type="submit">登録</v-btn>
+                  </v-form>
+                </v-container>
+              </v-sheet>
+            </v-container>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      valid: false,
+      firstName: '',
+      lastName: '',
+      nameRules: [
+        v => !!v || 'Name is required',
+      ],
+      email: '',
+      emailRules: [
+        v => !!v || 'E-mail is required',
+        v => /.+@.+/.test(v) || 'E-mail must be valid',
+      ],
+      password: '',
+      passwordRules: [
+        v => !!v || 'Password is required',
+      ]
+    }),
+    methods: {
+      logIn() {
+        this.$auth.loginWith('local',{
+          data: {
+            email: this.email,
+            password: this.password,
+          }
+        })
+      }
+    },
+  }
+</script>
