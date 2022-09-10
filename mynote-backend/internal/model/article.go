f15b71b7fba786e6ba4adb464f0f50ca5ba7a281
Masato Yamashita <4653960+M-Yamashita01@users.noreply.github.com>
2022-09-10 10:09:04 +0900

    Implement find articles and create article records.

--- mynote-backend/internal/model/article.go
+++ mynote-backend/internal/model/article.go
@@ -1,6 +1,10 @@
 package model
 
-import "time"
+import (
+	"MyNote/pkg/database"
+	"log"
+	"time"
+)
 
 type Article struct {
 	ID                      uint `gorm:"primary_key;AUTO_INCREMENT;not null;"`
@@ -10,4 +14,48 @@ type Article struct {
 	RelationArticleCategory []RelationArticleCategory
 	UserId                  uint `gorm:"not null"`
 	CreatedAt               time.Time
+}
+
+func CreateArticle(title string, url string, webSiteName string, userId uint) (*Article, error) {
+	db, err := database.DbInit()
+	if err != nil {
+		log.Println("Failed db connection.")
+		log.Println(err)
+		return &Article{}, err
+	}
+
+	defer database.Close(db)
+
+	article := Article{
+		Title:       title,
+		Url:         url,
+		WebsiteName: webSiteName,
+		UserId:      userId,
+	}
+
+	if err := db.Create(&article).Error; err != nil {
+		log.Println("Error ocurred.")
+		log.Println(err)
+		return &Article{}, err
+	}
+
+	return &article, nil
+}
+
+func FindArticlesSinceId(userId uint, sinceId uint, articleCount int) (*[]Article, error) {
+	db, err := database.DbInit()
+	if err != nil {
+		log.Println("Failed db connection.")
+		log.Println(err)
+		return nil, err
+	}
+
+	defer database.Close(db)
+
+	articles := []Article{}
+	if err := db.Where("user_id = ? AND id > ?", userId, sinceId).Order("id desc").Limit(articleCount).Find(&articles).Error; err != nil {
+		return nil, err
+	}
+
+	return &articles, nil
 }
---
+++ mynote-backend/internal/model/article_test.go
@@ -0,0 +1,63 @@
+package model_test
+
+import (
+	"MyNote/internal/model"
+	"MyNote/pkg/database/database_test"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Article", Ordered, func() {
+	var db *database_test.DB
+
+	BeforeAll(func() {
+		db = database_test.ConnectTestDb()
+
+		DeferCleanup(func() {
+			db.CloseTestDb()
+		})
+	})
+
+	Describe("FindArticlesSinceId", Ordered, func() {
+		Context("When an article exist", func() {
+			var userId uint
+
+			BeforeEach(func() {
+				user, _ := model.CreateUser()
+				userId = user.ID
+				model.CreateArticle("TestTitle", "https://www.example.com/", "example.com", userId)
+				model.CreateArticle("TestTitle2", "https://www.example.com/", "example.com", userId)
+			})
+
+			AfterEach(func() {
+				db.TruncateAllTable()
+			})
+
+			It("return two records of articles.", func() {
+				sinceId := 0
+				articleCount := 10
+				articles, _ := model.FindArticlesSinceId(userId, uint(sinceId), articleCount)
+				Expect(len((*articles))).To(Equal(2))
+
+				firstArticle := (*articles)[0]
+				Expect(firstArticle.Title).To(Equal("TestTitle2"))
+				secondArticle := (*articles)[1]
+				Expect(secondArticle.Title).To(Equal("TestTitle"))
+			})
+
+			Context("specified sinceId greater than zero", func() {
+				It("return articles with ids greater than sinceId.", func() {
+					sinceId := 1
+					articleCount := 10
+					articles, _ := model.FindArticlesSinceId(userId, uint(sinceId), articleCount)
+					Expect(len((*articles))).To(Equal(1))
+
+					article := (*articles)[0]
+					Expect(article.Title).To(Equal("TestTitle2"))
+				})
+			})
+			// TODO Write tests for checking limit 10.
+		})
+	})
+})
